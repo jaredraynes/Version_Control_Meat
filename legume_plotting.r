05de04e0ee70be82cb67464675ee9aa205c464e2
Jared Raynes <ray07c@csiro.au>
2018-03-22 12:11:14 +1100

    Added another box plot

--- legume_plotting.r
+++ legume_plotting.r
@@ -14,10 +14,20 @@ ggplot(legume, aes(x = Strain, y = weight)) +
   #scale_y_log10() +
   facet_wrap(~Plant)
 
+#Strain vs Weight showing effect of Plant Type
 ggplot(legume, aes(x = Strain, y = weight, colour = Strain)) +
   geom_boxplot(show.legend=F) +
-  #scale_y_log10() +
   facet_wrap(~Plant) +
+  labs(x = "Strain", y = "Weight") +
+  theme(axis.text.x = element_text(angle = 90)) +
+  theme(axis.title.y = element_text(size = rel(1.6), angle = 90)) +
+  theme(axis.title.x = element_text(size = rel(1.6), angle = 00)) +
+  theme(strip.text = element_text(size=20))
+#Plant vs Weight showing effect of Strain
+ggplot(legume, aes(x = Plant, y = weight, colour = Plant)) +
+  geom_boxplot(show.legend=F) +
+  facet_wrap(~Strain) +
+  labs(x = "Plant Type", y = "Weight") +
   theme(axis.text.x = element_text(angle = 90)) +
   theme(axis.title.y = element_text(size = rel(1.6), angle = 90)) +
   theme(axis.title.x = element_text(size = rel(1.6), angle = 00)) +
