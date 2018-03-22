71a504bb55550e41c576ea2dd2982b7cd73c6a55
Jared Raynes <ray07c@csiro.au>
2018-03-22 12:02:47 +1100

    Added mutate pipe

--- legume_data_wrangling.r
+++ legume_data_wrangling.r
@@ -1,3 +1,7 @@
 library(tidyverse)
 
-legume <- read_csv("Data/legume_weights_rhizobia_20str.csv")
+legume <- read_csv("Data/legume_weights_rhizobia_20str.csv") %>% 
+mutate(Strain = as.character(Strain)) 
+
+
+ 
